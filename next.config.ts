6d856a1ddaaaf1733ecda7e4242c116ccece069e
sergio Fontalvo <serme1009@gmail.com>
2025-10-28 14:32:58 -0500

    change amplify.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,      // Buenas prácticas de desarrollo
-  output: "standalone",       //  Necesario para despliegue en Amplify o Docker
+  reactStrictMode: true,      // Buenas prácticas de desarrollo      //  Necesario para despliegue en Amplify o Docker
   images: {
     domains: [],              // Puedes agregar dominios si usas imágenes externas
   }
